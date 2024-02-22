bce5470b7ecef6f89bbad77cf110a74f503691c9
mobin-newroz <ahmobin@newroztech.com>
2024-02-22 16:16:17 +0600

    useEffect

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,10 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function Todo(){
     const [todo, setTodo] = useState('');
     const [warning, setWarning] = useState('');
     const [count, setCount] = useState(0);
+    const [date, setDate] = useState(new Date());
 
     const handleInput = (e) => {
         const inputValue = e.target.value;
@@ -12,8 +13,36 @@ function Todo(){
         setWarning(warning);
     }
 
+    const tick = () => {
+        setDate(new Date())
+    }
+
+    /*
+    * useEffect calls on any changes on documents.
+    * useEffect first parameter accept callback function which is works for changes
+    * useEffect second parameter accept an array is use for getting decision to control the effect is being called or not.
+    * useEffect second parameter accept an array and if the value is an empty array then it will set a value for first time then just changes effect.
+    * reference: learn with summit react tutorial series, lesson 20, (at 22 minutes)
+    * */
+    useEffect(() => {
+        document.title = `Clicked ${count} times`;
+    },[count])
+
+    useEffect(() => {
+        console.log("Timer set") //this console will fall on very first time. cause for second param of useEffect is []
+        const timer = setInterval(tick,1000)
+
+        /*
+        * is useEffect return a function, it works as unmount any state
+        * reference: learn with summit react tutorial series, lesson 20, (at 29 minutes)
+        * */
+        return ()=>{
+            clearInterval(timer)
+        }
+    },[])
     return (
         <div>
+            <h3>Time : {date.toLocaleTimeString()}</h3>
             <p>{todo}</p>
             <p>
                 <textarea name="todo" onChange={handleInput} value={todo} />
