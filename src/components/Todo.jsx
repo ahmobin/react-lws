e09bfd225c18ef2ff1589ccde80ed3fe6b47bc90
mobin-newroz <ahmobin@newroztech.com>
2024-02-22 15:24:58 +0600

    useState

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,14 +3,15 @@ import {useState} from "react";
 function Todo(){
     const [todo, setTodo] = useState('');
     const [warning, setWarning] = useState('');
+    const [count, setCount] = useState(0);
 
     const handleInput = (e) => {
         const inputValue = e.target.value;
         const warning = inputValue.includes('.js') ? 'You need JavaScript skill to complete the task. Do you have it?' : 'Good Job!';
-
         setTodo(inputValue);
         setWarning(warning);
     }
+
     return (
         <div>
             <p>{todo}</p>
@@ -19,6 +20,9 @@ function Todo(){
             </p>
             <hr/>
             <h2>{warning}</h2>
+
+            <h1>{count}</h1>
+            <button type="button" onClick={() => setCount((count) => count + 1)}>counter</button>
         </div>
     );
 }
